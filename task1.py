7e234ecca60c34edd24692de7d3f3a7e64cd479d
Sotnikov_Andrey <sav91987@ya.ru>
2023-04-09 11:03:00 +0300

    added task1

---
+++ task1.py
@@ -0,0 +1,6 @@
+number = input('Введите трехзначное число: ')
+if len(number) != 3:
+    print('Введенное число не трехначное')
+else:
+    sum = int(number[0]) + int(number[1]) + int(number[2])
+    print(f"Сумма цифр числа {number} равна {sum}")
